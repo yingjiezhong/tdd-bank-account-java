bdaf7653b34ae2da782fa502479d65fd87dcc97f
Yingjie Zhong <yingjie.zhong@barchart.com>
2021-09-15 07:59:46 -0400

    Yingjie - added balance in account

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,7 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    double balance;
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    //@Disabled
+    public void initialBalanceCheck() {
+        Account account = new Account();
+        assertThat(account.balance).isEqualTo(0.0);
+
     }
+
 }
